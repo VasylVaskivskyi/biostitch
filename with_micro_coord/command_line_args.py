10ca9b02ac312bd790db99075e221d0244b6aebf
Vasyl Vaskivskyi <vv3@internal.sanger.ac.uk>
2019-09-04 17:15:35 +0100

    added ome-tif, changed order of dimensions

--- with_micro_coord/command_line_args.py
+++ with_micro_coord/command_line_args.py
@@ -4,5 +4,8 @@ parser = argparse.ArgumentParser(description="Phenix image stittcher.\nPLEASE DO
 parser.add_argument('--xml', type=str, nargs='?', help='path to the xml file typically ../Images/Index.idx.xml')
 parser.add_argument('--img_dir', type=str, nargs='?', help='path to the directory with images')
 parser.add_argument('--out_dir', type=str, nargs='?', help='path to output directory')
-parser.add_argument('--main_channel', type=str, nargs='?', help='channel that will be used as basis for stitching')
+parser.add_argument('--main_channel', type=str, nargs='?', help='channel that will be used as basis for stitching e.g. DAPI')
+parser.add_argument('--make_preview', type=str, nargs='?', default='TRUE', help='BOOL, if true, will generate z-max projection of main_channel')
+parser.add_argument('--stitch_only', type=str, nargs='+', default=['ALL'], help='specify space separated channel names to stitch (e.g. "DAPI" "ALEXA 657") default is to use all cahnnels')
+
 arguments = parser.parse_args()
--- with_micro_coord/main.py
+++ with_micro_coord/main.py
@@ -2,6 +2,7 @@ import tifffile as tif
 import gc 
 import numpy as np
 
+from apeer_ometiff_library.omexmlClass import OMEXML
 from datetime import datetime
 from command_line_args import arguments
 from image_positions import load_xml_tag_Images, get_image_sizes, get_image_paths_for_fields_per_channel, get_image_paths_for_planes_per_channel
@@ -11,13 +12,21 @@ from stitch_images import stitch_images, stitch_big_image
 
 def main():
     st = datetime.now()
-    print('started', st)
+    print('\nstarted', st)
 
     xml_path = arguments.xml
     img_dir = arguments.img_dir
     img_out_dir = arguments.out_dir
     main_channel = arguments.main_channel
+    make_preview = arguments.make_preview.upper()
+    stitch_only = arguments.stitch_only
 
+    if stitch_only != ['ALL'] and len(stitch_only) == 1:
+        main_channel = stitch_only[0]
+    if not img_out_dir.endswith('/'):
+        img_out_dir = img_out_dir + '/'
+    if not img_dir.endswith('/'):
+        img_dir = img_dir + '/'
 
     '''
     xml_path = 'C:/Users/vv3/Desktop/Index.idx.xml'
@@ -31,31 +40,50 @@ def main():
     planes_path_list = get_image_paths_for_planes_per_channel(img_dir, tag_Images)
     ids, x_size, y_size = get_image_sizes(tag_Images, main_channel)
 
-    print('generating z-max preview')
-    z_max_img_list = create_z_projection_for_preview(main_channel, fields_path_list)
-    images = equalize_histograms(z_max_img_list)
-    z_proj = stitch_images(images, ids, x_size, y_size)
-    tif.imwrite(img_out_dir + 'preview.tif', z_proj)
-    print('preview is available at ' + img_out_dir + 'preview.tif')
+    if make_preview == 'TRUE':
+        print('generating z-max preview')
+        z_max_img_list = create_z_projection_for_preview(main_channel, fields_path_list)
+        images = equalize_histograms(z_max_img_list)
+        z_proj = stitch_images(images, ids, x_size, y_size)
+        tif.imwrite(img_out_dir + 'preview.tif', z_proj)
+        print('preview is available at ' + img_out_dir + 'preview.tif')
+        del images, z_proj, z_max_img_list, fields_path_list
+        gc.collect()
 
-    nrows,ncols = z_proj.shape
+    ncols = sum(x_size.iloc[0, :])
+    nrows = sum(y_size.iloc[:, 0])
     n_planes = len(planes_path_list[main_channel])
     n_channels = len(planes_path_list.keys())
-    
-    del images, z_proj, z_max_img_list, fields_path_list
-    gc.collect()
-    
-    final_image = np.zeros((n_planes, nrows, ncols, n_channels), dtype=np.uint16)
+    channel_names = planes_path_list.keys()
+
+    if stitch_only != ['ALL']:
+        n_channels = len(stitch_only)
+        channel_names = stitch_only
+
+    final_image = np.zeros((1, n_channels, n_planes, nrows, ncols), dtype=np.uint16)
+
+    o = OMEXML()
+    o.image().Name = 'stitching_result.tif'
+
+    ome = o.image().Pixels
+    ome.SizeX = ncols
+    ome.SizeY = nrows
+    ome.SizeZ = n_planes
+    ome.SizeC = n_channels
+    ome.SizeT = 1
+    ome.DimensionOrder = 'TCZXY'
+    ome.PixelType = 'uint16'
+
     c = 0
-    for channel in planes_path_list.keys():
-        final_image[:, :, :, c] = stitch_big_image(channel, planes_path_list, ids, x_size, y_size, img_out_dir)
+    for channel in channel_names:
+        final_image[0, c, :, :, :] = stitch_big_image(channel, planes_path_list, ids, x_size, y_size, img_out_dir)
         c += 1
 
     del ids, x_size, y_size
     gc.collect()
     final_path = img_out_dir + 'stitching_result.tif'
     print('writing final image')
-    tif.imwrite(final_path, final_image)
+    tif.imwrite(final_path, final_image, metadata={'axes':'TCZXY', 'ImageWidth':ncols, 'ImageLength':nrows}, description=o.to_xml())
 
     fin = datetime.now()
     print('elapsed time', fin-st)
